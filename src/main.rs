7c46e8ac9c1f5abae32c5148aa0fec2789c1a5cf
RobertOLtean314 <olteanrobertadrian314@gmail.com>
2026-02-15 13:51:33 +0200

    I've started laying out the problem. I've added 2 cargo crates that helps me with HTTP requests

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+/**
+ *
+ *
+ */
+
+// initialy wanted to go with f16 to use less memory for my variables but it seems this data type is unstable as pe this known issue
+// <https://github.com/rust-lang/rust/issues/116909>
+pub fn take_user_imput(x_coordinate: f32, y_coordinate: f32, input_file: String) {}
+
+// Malformed entries should cause the program to exit appropriately => Proper error handling
+
+// added reqwest and tokip crates so I can have access to simple HTTP interactions
+
+fn main() {
+    println!("Hello, world!");
+}
